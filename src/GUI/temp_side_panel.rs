af3f6dc09ab058ba245e3a9ebd7a85565b260168
giorebecchi <giovanni.rebecchi@studenti.unitn.it>
2025-01-23 14:15:14 +0100

    Changes to GUI

--- src/GUI/temp_side_panel.rs
+++ src/GUI/temp_side_panel.rs
@@ -1,7 +1,6 @@
 use bevy::{prelude::*, window::PrimaryWindow, winit::WinitSettings};
 use bevy_egui::{egui, EguiContexts, EguiPlugin};
 
-
 #[derive(Default, Resource)]
 struct OccupiedScreenSpace {
     left: f32,
@@ -9,14 +8,12 @@ struct OccupiedScreenSpace {
     right: f32,
     bottom: f32,
 }
-
+struct UserInput(pub String);
 
 const CAMERA_TARGET: Vec3 = Vec3::ZERO;
 
-
 #[derive(Resource, Deref, DerefMut)]
 struct OriginalCameraTransform(Transform);
-
 
 pub fn main() {
     App::new()
@@ -26,69 +23,69 @@ pub fn main() {
         .init_resource::<OccupiedScreenSpace>()
         .add_systems(Startup, setup_system)
         .add_systems(Update, ui_example_system)
-        .add_systems(Update, update_camera_transform_system)
         .run();
 }
-
 
 fn ui_example_system(
     mut is_last_selected: Local<bool>,
     mut contexts: EguiContexts,
     mut occupied_screen_space: ResMut<OccupiedScreenSpace>,
 ) {
-    let ctx = contexts.ctx_mut();
+
+    if let Some(context)=contexts.try_ctx_mut() {
+        let ctx = context;
 
 
-    occupied_screen_space.left = egui::SidePanel::left("left_panel")
-        .resizable(true)
-        .show(ctx, |ui| {
-            ui.label("Left resizeable panel");
-            if ui
-                .add(egui::widgets::Button::new("A button").selected(!*is_last_selected))
-                .clicked()
-            {
-                *is_last_selected = false;
-            }
-            if ui
-                .add(egui::widgets::Button::new("Another button").selected(*is_last_selected))
-                .clicked()
-            {
-                *is_last_selected = true;
-            }
-            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
-        })
-        .response
-        .rect
-        .width();
-    occupied_screen_space.right = egui::SidePanel::right("right_panel")
-        .resizable(true)
-        .show(ctx, |ui| {
-            ui.label("Right resizeable panel");
-            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
-        })
-        .response
-        .rect
-        .width();
-    occupied_screen_space.top = egui::TopBottomPanel::top("top_panel")
-        .resizable(true)
-        .show(ctx, |ui| {
-            ui.label("Top resizeable panel");
-            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
-        })
-        .response
-        .rect
-        .height();
-    occupied_screen_space.bottom = egui::TopBottomPanel::bottom("bottom_panel")
-        .resizable(true)
-        .show(ctx, |ui| {
-            ui.label("Bottom resizeable panel");
-            ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
-        })
-        .response
-        .rect
-        .height();
+        occupied_screen_space.left = egui::SidePanel::left("left_panel")
+            .resizable(true)
+            .show(ctx, |ui| {
+                ui.label("Left resizeable panel");
+                if ui
+                    .add(egui::widgets::Button::new("A button").selected(!*is_last_selected))
+                    .clicked()
+                {
+                    *is_last_selected = false;
+                }
+                if ui
+                    .add(egui::widgets::Button::new("Another button").selected(*is_last_selected))
+                    .clicked()
+                {
+                    *is_last_selected = true;
+                }
+                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+            })
+            .response
+            .rect
+            .width();
+        occupied_screen_space.right = egui::SidePanel::right("right_panel")
+            .resizable(true)
+            .show(ctx, |ui| {
+                ui.label("Right resizeable panel");
+                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+            })
+            .response
+            .rect
+            .width();
+        occupied_screen_space.top = egui::TopBottomPanel::top("top_panel")
+            .resizable(true)
+            .show(ctx, |ui| {
+                ui.label("Top resizeable panel");
+                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+            })
+            .response
+            .rect
+            .height();
+        occupied_screen_space.bottom = egui::TopBottomPanel::bottom("bottom_panel")
+            .resizable(true)
+            .show(ctx, |ui| {
+                ui.label("Bottom resizeable panel");
+                ui.allocate_rect(ui.available_rect_before_wrap(), egui::Sense::hover());
+            })
+            .response
+            .rect
+            .height();
+    }
 }
-
 
 fn setup_system(
     mut commands: Commands,
@@ -113,45 +110,11 @@ fn setup_system(
         Transform::from_xyz(4.0, 8.0, 4.0),
     ));
 
-
     let camera_pos = Vec3::new(-2.0, 2.5, 5.0);
     let camera_transform =
         Transform::from_translation(camera_pos).looking_at(CAMERA_TARGET, Vec3::Y);
     commands.insert_resource(OriginalCameraTransform(camera_transform));
 
-
     commands.spawn((Camera3d::default(), camera_transform));
 }
 
-
-fn update_camera_transform_system(
-    occupied_screen_space: Res<OccupiedScreenSpace>,
-    original_camera_transform: Res<OriginalCameraTransform>,
-    windows: Query<&Window, With<PrimaryWindow>>,
-    mut camera_query: Query<(&Projection, &mut Transform)>,
-) {
-    let (camera_projection, mut transform) = match camera_query.get_single_mut() {
-        Ok((Projection::Perspective(projection), transform)) => (projection, transform),
-        _ => unreachable!(),
-    };
-
-
-    let distance_to_target = (CAMERA_TARGET - original_camera_transform.translation).length();
-    let frustum_height = 2.0 * distance_to_target * (camera_projection.fov * 0.5).tan();
-    let frustum_width = frustum_height * camera_projection.aspect_ratio;
-
-
-    let window = windows.single();
-
-
-    let left_taken = occupied_screen_space.left / window.width();
-    let right_taken = occupied_screen_space.right / window.width();
-    let top_taken = occupied_screen_space.top / window.height();
-    let bottom_taken = occupied_screen_space.bottom / window.height();
-    transform.translation = original_camera_transform.translation
-        + transform.rotation.mul_vec3(Vec3::new(
-        (right_taken - left_taken) * frustum_width * 0.5,
-        (top_taken - bottom_taken) * frustum_height * 0.5,
-        0.0,
-    ));
-}
