7be9481aca093a17151ad75341732766f084ea38
chiara <chiara.strozzi@studenti.unitn.it>
2025-04-26 15:11:51 +0200

    deleted old commands

--- src/common_things/common.rs
+++ src/common_things/common.rs
@@ -47,7 +47,6 @@ pub enum ChatResponse{
     ForwardMessage(MessageChat)
 }
 
-
 #[derive(Serialize,Deserialize,Debug, Clone)]
 pub struct MessageChat{ //which needs to be fragmented
     //pub general: Packet,
@@ -69,37 +68,15 @@ pub enum ClientType{
     WebBrowser
 }
 
-// text/media server and client
 
-#[derive(Serialize, Deserialize, Debug)]
-pub enum RequestWeb {
-    ServerType,
-    TextList, //to retrieve text file list
-    TextFile (String), //title file
-    MediaList,
-    Media (String)
-}
-
-// server to client
-#[derive(Serialize, Deserialize, Debug)]
-pub enum WebResponse{
-    ServerType(ServerType),
-    FileList(Vec<String>),
-    File(String), // la stringa con tutto il file di testo
-    MediaList(Vec<String>),
-    Media (String),
-    Error(String),
-}
-
-
-//NEW DRAFT
+//COMMANDS TEXT, MEDIA SERVERS AND WEB BROWSER
 pub type MediaId = String;
 pub type TextId = String;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FileMetaData{
-    pub(crate) title: String, 
-    pub(crate) extension: String,
-    pub(crate) s_id: u64,
+    pub title: String,
+    pub extension: String,
+    pub s_id: u64,
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub enum WebBrowserCommands{
@@ -122,7 +99,7 @@ pub enum TextServer{
 
 #[derive(Serialize, Deserialize, Debug)]
 pub enum MediaServer{
-    ServerType(ServerType), //probably needed
+    ServerType(ServerType),
     SendPath(Vec<MediaId>), //send paths to the text server
     SendMedia(FileMetaData) //send correct media to the client who asked
 }
@@ -140,6 +117,6 @@ pub enum ContentCommands{
     Crash
 }
 
-pub enum WebBrowserEvents{
+pub enum WebBrowserEvents{ //not complete
     TypeClient(ClientType, NodeId)
 }
