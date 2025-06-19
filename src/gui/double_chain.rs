aa65a4362a942559b833701cd1054d6165b34cc7
giorebecchi <giovanni.rebecchi@studenti.unitn.it>
2025-06-19 14:52:56 +0200

    fixed double_chain configuration for full topology

--- src/gui/double_chain.rs
+++ src/gui/double_chain.rs
@@ -10,58 +10,61 @@ pub fn spawn_double_chain(
     let config = parse_config();
     let node_count = config.client.len() + config.server.len() + config.drone.len();
 
+    let horizontal_spacing = 80.0;
+    let vertical_spacing = 120.0;
 
-    let nodes_in_first_row = (node_count + 1) / 2;
-    let nodes_in_second_row = node_count - nodes_in_first_row;
-
-    let horizontal_spacing = 100.0;
-    let vertical_spacing = 100.0;
-
-    let first_row_y = vertical_spacing / 2.0;
-    let second_row_y = -vertical_spacing / 2.0;
-
-    let mut nodes = Vec::with_capacity(node_count);
     let mut all_nodes = Vec::with_capacity(node_count);
-
 
     for drone in &config.drone {
         all_nodes.push((NodeType::Drone, drone.id, &drone.connected_node_ids, drone.pdr));
     }
 
     for client in &config.client {
-        for (client_type, id) in &clients.clients {
-            if id.clone() == client.id {
-                match client_type {
-                    NodeType::WebBrowser => all_nodes.push((NodeType::WebBrowser, client.id, &client.connected_drone_ids, -1.00)),
-                    NodeType::ChatClient => all_nodes.push((NodeType::ChatClient, client.id, &client.connected_drone_ids, -1.00)),
-                    _ => unreachable!()
-                }
+        if let Some((client_type, _)) = clients.clients.iter()
+            .find(|(_, id)| *id == client.id) {
+            match client_type {
+                NodeType::WebBrowser => all_nodes.push((NodeType::WebBrowser, client.id, &client.connected_drone_ids, -1.00)),
+                NodeType::ChatClient => all_nodes.push((NodeType::ChatClient, client.id, &client.connected_drone_ids, -1.00)),
+                _ => unreachable!()
             }
         }
     }
 
     for server in &config.server {
-        for (server_type, id) in &clients.servers {
-            if id.clone() == server.id {
-                match server_type {
-                    NodeType::TextServer => all_nodes.push((NodeType::TextServer, server.id, &server.connected_drone_ids, -1.00)),
-                    NodeType::MediaServer => all_nodes.push((NodeType::MediaServer, server.id, &server.connected_drone_ids, -1.00)),
-                    NodeType::ChatServer => all_nodes.push((NodeType::ChatServer, server.id, &server.connected_drone_ids, -1.00)),
-                    _ => unreachable!()
-                }
+        if let Some((server_type, _)) = clients.servers.iter()
+            .find(|(_, id)| *id == server.id) {
+            match server_type {
+                NodeType::TextServer => all_nodes.push((NodeType::TextServer, server.id, &server.connected_drone_ids, -1.00)),
+                NodeType::MediaServer => all_nodes.push((NodeType::MediaServer, server.id, &server.connected_drone_ids, -1.00)),
+                NodeType::ChatServer => all_nodes.push((NodeType::ChatServer, server.id, &server.connected_drone_ids, -1.00)),
+                _ => unreachable!()
             }
         }
     }
 
-    for (i, (node_type, id, connected_ids, pdr)) in all_nodes.iter().enumerate() {
-        let (row, nodes_in_row, position_in_row) = if i < nodes_in_first_row {
-            (0, nodes_in_first_row, i)
-        } else {
-            (1, nodes_in_second_row, i - nodes_in_first_row)
-        };
+    let top_count = node_count / 2 + 1;
+    let bottom_count = node_count / 2;
 
-        let x = (position_in_row as f32 - (nodes_in_row - 1) as f32 / 2.0) * horizontal_spacing;
-        let y = if row == 0 { first_row_y } else { second_row_y };
+    let (top_nodes, bottom_nodes) = all_nodes.split_at(top_count);
+
+    let mut nodes = Vec::with_capacity(node_count);
+
+    for (i, (node_type, id, connected_ids, pdr)) in top_nodes.iter().enumerate() {
+        let x = (i as f32 - (top_count - 1) as f32 / 2.0) * horizontal_spacing;
+        let y = vertical_spacing / 2.0;
+
+        nodes.push(NodeConfig::new(
+            node_type.clone(),
+            *id,
+            Vec2::new(x, y),
+            (*connected_ids).clone(),
+            *pdr
+        ));
+    }
+
+    for (i, (node_type, id, connected_ids, pdr)) in bottom_nodes.iter().enumerate() {
+        let x = (i as f32 - (bottom_count - 1) as f32 / 2.0) * horizontal_spacing;
+        let y = -vertical_spacing / 2.0;
 
         nodes.push(NodeConfig::new(
             node_type.clone(),
